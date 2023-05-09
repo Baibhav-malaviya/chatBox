1844e42c273cc63a2de4ea0fb1c43428c765aaee
Baibhav Kumar <baibhav.kr73@gmail.com>
2023-05-09 23:24:15 +0530

    minimum width of text is set.

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express  = require('express');
+const serverless = require('serverless-http');
+const os= require('os');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+
+
+const primaryUid=os.userInfo().uid;
+if(primaryUid != os.userInfo().uid) {
+    const secondaryUid=os.userInfo().uid;
+}
+//? code from line 10-13 identify both the user as primary and secondary
+
+// Just checking the os modules
+
+console.log(`The userInfo of this device is ${os.userInfo().uid}`);
 
 app.use(express.static ('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,6 +49,13 @@ app.post('/',(req,res)=>{
 
 });
 
-app.listen(5000,()=>{
+// const router = express.Router();
+
+
+
+// app.use('/.netlify/functions/app',router)
+
+// module.exports.handeler = serverless(app);
+app.listen(process.env.PORT || 5000,()=>{
     console.log("Listening on port 5000");
 })
